49b8c072ec1a547dc95d6ea62ae2891eba3e5ad8
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2025-09-03 21:25:32 +0530

    env errors

--- src/pages/SharedNotes.jsx
+++ src/pages/SharedNotes.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const SharedNotes = () => {
   const { shareId } = useParams(); 
-  const frontend_url = import.meta.env.VITE_FRONTEND_URL;
+  const frontend_url = 'https://notes-app-woad-psi.vercel.app'
   
 
   const [note, setNote] = useState(null);
