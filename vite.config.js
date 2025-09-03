1c69a27b8cf6b1aa2422839d7d9635a7d4c4a42a
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2025-09-03 21:10:45 +0530

    All good

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/NotesAppClient/',
-  plugins: [react(),tailwindcss()],
+  base: './',
+  plugins: [react(), tailwindcss()]
 })
